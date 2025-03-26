d433e1ae68ef6d795b310552b66237ecec879a8a
thegreek97 <francesco.greco1697@gmail.com>
2025-03-26 15:00:39 +0100

    Add training Model

---
+++ database/migrations/2025_03_25_135853_create_trainings_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trainings', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->longText("introduction")->nullable();
+            $table->longText("scenario")->nullable();
+            $table->longText("defense_strategies")->nullable();
+            $table->longText("exercises")->nullable();
+            $table->longText("conclusions")->nullable();
+
+            $table->boolean("completed")->default(false);
+            $table->foreignId("user_id")->constrained("users");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trainings');
+    }
+};
