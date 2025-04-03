d827fc8fb84c77410d63738c96790b309f5ed533
thegreek97 <francesco.greco1697@gmail.com>
2025-04-03 14:50:27 +0200

    Remove buggy validation training reaction questionnaire

--- app/Http/Controllers/TrainingReactionController.php
+++ app/Http/Controllers/TrainingReactionController.php
@@ -13,7 +13,22 @@ class TrainingReactionController extends Controller
     public function create(Request $request)
     {
         $user = Auth::user();
-        $validatedData = $request->validate([
+        $questionnaire = new TrainingReaction();
+        $questionnaire->user_id = $user->id;
+
+        $questionnaire->q1 = $request->input('q1');
+        $questionnaire->q2 = $request->input('q2');
+        $questionnaire->q3 = $request->input('q3');
+        $questionnaire->q4 = $request->input('q4');
+        $questionnaire->q5 = $request->input('q5');
+        $questionnaire->q6 = $request->input('q6');
+        $questionnaire->q7 = $request->input('q7');
+        $questionnaire->q8 = $request->input('q8');
+        $questionnaire->trivial_question = $request->input('trivial_question');
+        $questionnaire->fastClickCount = $request->input('fastClickCount');
+        $questionnaire->save();
+
+        /*$validatedData = $request->validate([
             'q1' => ['required', 'integer'],
             'q2' => ['required', 'integer'],
             'q3' => ['required', 'integer'],
@@ -34,6 +49,7 @@ class TrainingReactionController extends Controller
             $validatedData['user_id'] = $user->id;
             TrainingReaction::create($validatedData);
         }
+        */
 
         $user->training_reaction_completed = now();
         $user->save();
