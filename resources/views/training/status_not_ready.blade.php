3350a39bc15d022e966ba368d8e4ac5ed9cf5741
TheGreek97 <francesco.greco1697@gmail.com>
2025-07-11 14:13:06 +0000

    Merge server changes

--- resources/views/training/status_not_ready.blade.php
+++ resources/views/training/status_not_ready.blade.php
@@ -2,8 +2,9 @@
     <x-slot name="slot">
         <div class="min-h-screen text-2xl bg-gray-200 flex justify-center items-center" style="flex-direction: column">
             <p class="flex font-bold">⚙️ Training Status: </p>
-            <p>We're still generating the training material for you...<br>
-                Please try to refresh the page in about 2 minutes.</p>
+            <p style="margin: 3%">You will shortly have to attend a brief training course as part of this study. <br>
+		However, we're still generating the training material for you...<br>
+                Please try to refresh the page in some minutes. Thank you for your patience.</p>
         </div>
     </x-slot>
 </x-app-layout>
